337de571f24898a664691cfae02df453f04525f2
jetztmachtsbumm <68288315+jetztmachtsbumm@users.noreply.github.com>
2023-09-24 00:26:31 +0200

    Camera movement

--- ScrabbleOnline/Assets/Scripts/PlayerControlls.cs
+++ ScrabbleOnline/Assets/Scripts/PlayerControlls.cs
@@ -8,6 +8,11 @@ public class PlayerControlls : MonoBehaviour
 
     public static PlayerControlls Instance { get; private set; }
 
+    [SerializeField] Transform cameraTransform;
+    [SerializeField] float cameraSpeed = 1f;
+    [SerializeField] float cameraZoomSpeed = 1f;
+    [SerializeField] float cameraMaxHeight, cameraMinHeight;
+
     void Awake()
     {
         if (Instance != null)
@@ -26,7 +31,11 @@ public class PlayerControlls : MonoBehaviour
             {
                 BrickGhost.Instance.CreateBrickVisualServerRpc(GridSystem.Instance.GetCellAtPosition(MouseWorld.Instance.GetMouseWorldPosition()));
             }
-        }    
+        }
+
+        float cameraZoom = Input.mouseScrollDelta.y * cameraZoomSpeed * 1000 * Time.deltaTime;
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * cameraSpeed * Time.deltaTime;
+        cameraTransform.position = new Vector3(cameraTransform.position.x + movement.x, Mathf.Clamp(cameraTransform.position.y - cameraZoom, 5, 50), cameraTransform.position.z + movement.z);
     }
 
 }
